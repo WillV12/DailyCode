d5e2a58a0b186a966e7a881282af27620c8d2a66
WillV12 <thatmurphyboi@gmail.com>
2025-01-17 08:37:44 -0500

    Daily code - js&html

---
+++ prime.js
@@ -0,0 +1,28 @@
+let input = document.getElementById("input");
+let output = document.getElementById("output");
+let form = document.getElementById("inputForm")
+
+form.addEventListener('submit', (event) =>{
+    
+    event.preventDefault()
+
+    let userInput = input.value 
+    let isPrime
+
+    for (let i = 1; i <= userInput; i++){
+    
+        if ((userInput % i == 0) && (i != userInput && i != 1)){
+            isPrime = false
+            break
+        }
+    }
+
+    if (isPrime == false){
+        window.alert("The number is not prime")
+    }
+    else{
+        window.alert("The number is prime")
+    }
+});
+
+
